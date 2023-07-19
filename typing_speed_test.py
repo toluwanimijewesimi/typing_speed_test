16b74c98719c15a5614db5830aa70cd62284aa3b
Tolwaunimi jewesimi <toluwanimi.jewesimi@gmail.com>
2023-07-18 22:24:21 -0500

    speed typing test first draft; can type letters and pressing space clears line

--- typing_speed_test.py
+++ typing_speed_test.py
@@ -1,17 +1,58 @@
 import curses
 
-stdscr = curses.initscr()
-curses.start_color()
 
-curses.noecho()
+stdscr = curses.initscr()
+answer = curses.newwin(15,15, 12,0)
+#curses.start_color
+
+#curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
 
 stdscr.clear()
-curses.init_pair(1, 1, 2)
-stdscr.addstr (10,0, "Hello World!", curses. color_pair(1))
+word = "Hello World!"
+stdscr.addstr(10,0, word)
 stdscr.refresh()
+
+correct = []
+"""
+x = 0
+
+for let in word:
+    a = answer.getch(12,x)
+    x += 1
+    stdscr.move(12,x)
+    stdscr.refresh()
+    
+    #if a == ord("b"):
+        #stdscr.addstr(10,0, word, curses.A_STANDOUT)
+        #stdscr.refresh()
+    
+    if a == ord(" "):
+        answer.clear()
+        x = 0
+        answer.refresh()
+"""
+
+x = 0
+y = len(word)
+z = y
+q = 0
+while z != 0:
+    a = answer.getch(12,x)
+    x += 1
+    if a == ord(word[q]):
+        q += 1
+        z -= 1
+        if a == ord(" "):
+            answer.clear()
+            x = 0
+            answer.refresh()
+
+
+
+
 
 curses.nocbreak()
 stdscr.keypad(False)
-curses.echo()
+#curses.echo()
