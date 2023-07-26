0c2e7d365e324b5a6a8962e08cab7076bd26d211
Tolwaunimi jewesimi <toluwanimi.jewesimi@gmail.com>
2023-07-26 00:19:58 -0500

    cleaned up code and added comments

--- typing_speed_test.py
+++ typing_speed_test.py
@@ -1,6 +1,5 @@
 import curses
 
-
 stdscr = curses.initscr()
 answer = curses.newwin(15,15, 12,0)
 #curses.start_color
@@ -15,64 +14,38 @@ sentence = "The quick brown fox..."
 stdscr.addstr(10,0, sentence)
 stdscr.refresh()
 
-"""
-x = 0
+li_sentence = [] #empty list for all the letters in the sentece, in order
+typed = [] #empty list for all the letters the user has tpyed
 
-for let in word:
-    a = answer.getch(12,x)
-    x += 1
-    stdscr.move(12,x)
-    stdscr.refresh()
-    
-    #if a == ord("b"):
-        #stdscr.addstr(10,0, word, curses.A_STANDOUT)
-        #stdscr.refresh()
-    
-    if a == ord(" "):
-        answer.clear()
-        x = 0
-        answer.refresh()
-"""
-#def current_word(setence):
-    #hello
-
-'''
-for letter in sentence:
-    if letter == " ":
-        words.append(letter)
-    else:
-        hold = letter
-        check = letter
-        while check != " ":
-            check += 1
-            if check == " ":
-                letter = check
-        words.append(sentence[hold, check])
-'''
-li_sentence = []
-typed = []
-
+#appends all letters in sentence to the appropriate list
 for lett in sentence:
     li_sentence.append(ord(lett))
 
-cursor = 0
+cursor = 0 #starting point for cursor
 sentence_len = len(sentence)
-sentence_index = 0
+sentence_index = 0 #tracks what letter the user needs to type currently
+
+#allows user to enter letters until they have typed all the letters
 while sentence_len != 0:
     type = answer.getch(12,cursor)
-    cursor += 1
+    cursor += 1 #incriments cursor to display next letter next to the previous letter
     typed.append(type)
+    #removes all the backspace inputs that are automatically added to the tyed list
     for errorz in typed:
         if errorz == 127:
             typed.remove(errorz)
+    #checks if entered letter matches expeted letter
     if type == ord(sentence[sentence_index]):
+        #makes sure the complete sence is acurate and not jsut the current letter by comparing the list of typed letters and expected letters
         if typed == li_sentence[:sentence_index + 1]:
-            sentence_index += 1
-            sentence_len -= 1
+            sentence_index += 1 #moves expected letter to next letter
+            sentence_len -= 1 #reduces letters needed to be typed sentece is complted
+            #clears line if previous word was spelt correclty and funtions as the space in the sentence
             if type == ord(" "):
                 answer.clear()
                 cursor = 0
                 answer.refresh()
+    #allows backspace to be used in terminal and removes the backspaced letter from letters in list of typed letters
     if type == 127:
         answer.delch(12, cursor)
         answer.delch(12, cursor - 1)
@@ -80,22 +53,6 @@ while sentence_len != 0:
         cursor -= 2
         typed.pop()
         answer.refresh()
-'''
-#incorporate into main code
-index_ = 0
-checker = 1
-while index_ != len(typed) + 1:
-    for let in typed:
-        if let == letters[index_]:
-            index_ += 1
-        else:
-            checker = -1
-    if checker == 1:
-        print("True")
-            
-'''
- 
-
 
 curses.nocbreak()
 stdscr.keypad(False)
