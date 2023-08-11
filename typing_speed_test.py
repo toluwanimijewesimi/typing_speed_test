2353ca925479d733ed1db8d56069f0d6e420e381
Tolwaunimi jewesimi <toluwanimi.jewesimi@gmail.com>
2023-08-11 08:50:21 -0500

    added/edited comments, deleted redundant code, edited variable names, added new senteces to sentencs.txt

--- typing_speed_test.py
+++ typing_speed_test.py
@@ -2,6 +2,7 @@ import curses
 import time
 from random import choice
 
+#initialize windows
 stdscr = curses.initscr()
 wpm_win = curses.newwin(3, 90, 0, 0)
 display = curses.newwin(6, 90, 3, 0)
@@ -9,8 +10,7 @@ answer = curses.newwin(3, 90, 8, 0)
 curses.start_color()
 curses.use_default_colors()
 
-wpm_win.refresh()
-
+#intializing some comands
 #curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
@@ -19,11 +19,14 @@ stdscr.keypad(True)
 curses.init_pair(1, 2, -1)
 curses.init_pair(2, 7, 1)
 
+#print starting screen
 start = True
 play = False
 stdscr.clear()
 wpm_win.addstr(1, 1, 'Welcome to the Typing Speed Test \n Press space to start the test, or the ">" key at any time to quit.')
 wpm_win.refresh()
+
+#check wether playing on quiting
 wait = 0
 while wait != ord (" ") and wait != (">"):
     wait = answer.getch(1, 1)
@@ -36,15 +39,12 @@ while wait != ord (" ") and wait != (">"):
         wpm_win.refresh()
         break
 
-
+#while loop to set up the test
 while play == True:
     with open("sentences.txt") as file:
         sentences = file.readlines()
         sentence = choice(sentences)
         sentence = sentence.strip()
-        #sentence = "Filler"
-
-    #word = "Hello World!"
 
     li_sentence = [] #empty list for all the letters in the sentece, in order
     typed = [] #empty list for all the letters the user has tpyed
@@ -57,7 +57,6 @@ while play == True:
     #print out sentence list one letter at a time
     y = 1
     x = 1
-
     for a in li_sentence:
         a = chr(a)
         display.addstr(y,x, a)
@@ -68,10 +67,11 @@ while play == True:
         display.refresh()
         x += 1
 
-    wpm_win.addstr(1, 1, "WMP: 0")
-    wpm_win.addstr(1, 11, f"Accuracy: 0%")
+    #print out WMP and Accuracy
     wpm = 0
     accuracy = 0
+    wpm_win.addstr(1, 1, f"WMP: {wpm}")
+    wpm_win.addstr(1, 11, f"Accuracy: {accuracy}%")
 
     wpm_win.refresh()
 
@@ -79,14 +79,16 @@ while play == True:
     sentence_len = len(sentence)
     sentence_index = 0 #tracks what letter the user needs to type currently
 
+    #tracks typed an correct letters to calculate accuracy
     typed_count = 0
     correct_count = 0
 
-    start_time = time.time()
+    start_time = time.time() #starts timer for game
 
 
-    #allows user to enter letters until they have typed all the letters
+    #while loop to allow user to enter letters until they have typed all the letters
     while True:
+        #takes in user inout and exits game if needed
         try:
             type = answer.getch(1,cursor)
             if type == ord(">"):
@@ -95,19 +97,18 @@ while play == True:
             break
         cursor += 1 #incriments cursor to display next letter next to the previous letter
         typed.append(type)
-        #dont add deletes to the coun
+        #doesnt add deleted text to the count
         if type != 127:
             typed_count += 1
-        #removes all the backspace inputs that are automatically added to the tyed list
-        for errorz in typed:
-            if errorz == 127:
-                typed.remove(errorz)
+        #removes all the backspace inputs that are automatically added to the typed list
+        for mistakes in typed:
+            if mistakes == 127:
+                typed.remove(mistakes)
         #checks if entered letter matches expected letter
         if type == ord(sentence[sentence_index]):
             #makes sure the complete sentence is acurate and not jsut the current letter by comparing the list of typed letters and expected letters
             if typed == li_sentence[:sentence_index + 1]:
                 sentence_index += 1 #moves expected letter to next letter
-                sentence_len -= 1 #reduces letters needed to be typed sentece is complted
                 correct.append(type)
                 correct_count += 1
                 #clears line if previous word was spelt correclty and funtions as the space in the sentence
@@ -119,10 +120,12 @@ while play == True:
             curses.beep()
         #allows backspace to be used in terminal and removes the backspaced letter from letters in list of typed letters
         if type == 127:
+            #if there is nothing to backspace, it doesnt backspace
             if cursor == 1 or cursor == 2:
                 answer.clear()
                 answer.refresh()
                 cursor = 1
+            #backspaces if there is text to backspace
             else:
                 answer.delch(1, cursor)
                 answer.delch(1, cursor - 1)
@@ -132,21 +135,13 @@ while play == True:
                     typed.pop()
                     correct.pop()
                     sentence_index -= 1
-                    sentence_len += 1
                 else:
                     typed.pop()
                 answer.refresh()
         #highlight text based on wether the input is correct or not and add moving cursor
-        
-        #display.clear()
-
-        
-
-        #print correct as green
-        #display.clear()
+        #print correct text as green
         y1 = 1
         x1 = 1
-    
         for l in sentence[:len(correct)]:
             display.chgat(y1, x1, 1, curses.color_pair(1))
             if l == " ":
@@ -156,7 +151,7 @@ while play == True:
             display.refresh()
             x1 += 1
         
-        #print wrong as red
+        #print wrong text as red
         y2 = y1
         x2 = x1
         for l in sentence[len(correct):len(typed)]:
@@ -168,10 +163,9 @@ while play == True:
                 display.refresh()
                 x2 += 1
 
-        #print cursor
+        #print moving cursor
         y3 = y2
         x3 = x2
-        #for l in sentence[len(typed)]:
         display.chgat(y3, x3, 1, curses.A_REVERSE)
         if l == " ":
             if x3 > 70 and x3 < 70:
@@ -183,7 +177,6 @@ while play == True:
         #clear deleted cursor
         y4 = y3
         x4 = x3
-        #for l in sentence[len(typed)]:
         display.chgat(y4, x4, 1, curses.color_pair(0))
         if l == " ":
             if x4 > 70 and x4 < 70:
@@ -199,7 +192,7 @@ while play == True:
         sec_to_min = elapsed_time/60
         wpm = (len(correct)/5)/sec_to_min
         wpm = round(wpm)
-
+        
         #calculate accuracy
         accuracy = (correct_count/typed_count) * 100
         accuracy = round(accuracy)
@@ -210,16 +203,19 @@ while play == True:
         wpm_win.addstr(1, 11, f"Accuracy: {accuracy}%")
         wpm_win.refresh()
 
+        #end program
         if len(correct) == len(sentence):
             answer.clear()
             break
 
-
+    #print replay/ending screen
     wpm_win.clear()
     wpm_win.addstr(1, 1, f"WMP: {wpm}")
     wpm_win.addstr(1, 11, f"Accuracy: {accuracy}%")
     answer.addstr(2, 1, 'Great job, enter space to play again or the ">" key to quit.')
     wpm_win.refresh()
+
+    #check wether replaying or ending
     wait = 0
     wait = answer.getch(1, 1)
     if wait == ord(" "):
@@ -227,25 +223,14 @@ while play == True:
         wpm_win.clear()
         answer.clear()
         display.clear()
-    else:
+    elif wait == ord(">"):
         start == False
         wpm_win.clear()
         wpm_win.addstr(1, 0, "Thanks for playing!")
         wpm_win.refresh()
         break
 
-    
-
-
-
-    
-
-    #display.addstr(y, x, sentence[:len(correct)], curses.color_pair(1))
-    #display.addstr(1, len(correct) + 1, senence[len(correct):len(typed)], curses.color_pair(2))
-    #display.addstr(1, len(typed) + 1, sentence[len(typed)], curses.A_REVERSE)
-    #display.addstr(1, len(typed) + 2, sentence[len(typed) + 1:])
-
-
+#terminating some comands
 curses.nocbreak()
 stdscr.keypad(False)
 #curses.echo()
